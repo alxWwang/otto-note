731a4def5e7c8a9ca2debe1abda5381b0454d136
Nicholas Wang <nawang2@iastate.edu>
2025-01-14 00:30:28 -0600

    file system parsing works

--- Frontend/app/Editor.tsx
+++ Frontend/app/Editor.tsx
@@ -2,13 +2,15 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import FolderItem from '@/components/Homepage/FolderItem';
 
-export default function TabTwoScreen() {
+export default function Editor() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Editor</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <EditScreenInfo path="app/(tabs)/yes.tsx" />
+      
     </View>
   );
 }
